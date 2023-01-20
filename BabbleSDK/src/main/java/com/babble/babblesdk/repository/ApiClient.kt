383e109a4ce2f81ab572675be20acd908b0c369c
Nirmal <nirmalvora61@gmail.com>
2023-01-20 09:14:01 +0530

    api base url change

--- BabbleSDK/src/main/java/com/babble/babblesdk/repository/ApiClient.kt
+++ BabbleSDK/src/main/java/com/babble/babblesdk/repository/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object ApiClient {
-    private const val baseUrl = "https://b52c-103-100-17-93.in.ngrok.io/"
+    private const val baseUrl = "https://1d0f-103-100-17-103.in.ngrok.io/"
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
