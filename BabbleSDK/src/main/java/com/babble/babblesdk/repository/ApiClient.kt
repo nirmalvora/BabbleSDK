05a189884015a8c0afcbbc83925a90e5aae34a3b
Nirmal <nirmalvora61@gmail.com>
2023-02-17 11:04:49 +0530

    Update ApiClient.kt

--- BabbleSDK/src/main/java/com/babble/babblesdk/repository/ApiClient.kt
+++ BabbleSDK/src/main/java/com/babble/babblesdk/repository/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object ApiClient {
-    private const val baseUrl = "https://pri9rvopx2.execute-api.ap-south-1.amazonaws.com/prod-2/"
+    private const val baseUrl = "https://babble-app-backend.herokuapp.com/"
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
