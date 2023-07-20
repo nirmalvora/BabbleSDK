44bf0d7d7b911ba70c7e359182ceae2b5533acc8
Satish <nirmalvora61@gmail.com>
2023-07-20 17:19:21 +0530

    Revert "Review removed"
    This reverts commit b76a9f8a91de44947f417b16c66bc354c63838e3.

--- BabbleSDK/src/main/java/com/babble/babblesdk/ui/SurveyActivity.kt
+++ BabbleSDK/src/main/java/com/babble/babblesdk/ui/SurveyActivity.kt
@@ -34,6 +34,7 @@ import com.babble.babblesdk.ui.fragments.BabbleQueTextFragment
 import com.babble.babblesdk.ui.fragments.BabbleWelcomeFragment
 import com.babble.babblesdk.utils.BabbleConstants
 import com.babble.babblesdk.utils.BabbleSdkHelper
+import com.google.android.play.core.review.ReviewManagerFactory
 import com.google.gson.Gson
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -319,6 +320,23 @@ class SurveyActivity : AppCompatActivity() {
             )?.get("fields")?.get(checkForNextQuestion)?.get("stringValue")
                 ?: "").lowercase() == "in_app_survey"
         ) {
+            val manager = ReviewManagerFactory.create(this)
+            val request = manager.requestReviewFlow()
+            request.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // We got the ReviewInfo object
+                    val reviewInfo = task.result
+                    val flow = manager.launchReviewFlow(this, reviewInfo)
+                    flow.addOnCompleteListener {
+                        // The flow has finished. The API does not indicate whether the user
+                        // reviewed or not, or even whether the review dialog was shown. Thus, no
+                        // matter the result, we continue our app flow.
+                    }
+                } else {
+                    // There was some problem, log or handle the error code.
+                    Log.e(TAG, "triggerSurvey: ${task.exception}")
+                }
+            }
             return false
         } else if ((surveyResponse.document?.fields?.nextQuestion?.get(
                 "mapValue"
